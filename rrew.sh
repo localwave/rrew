a33142920ecc29aae000835ccf3a480ab6397777
Larry Jover <136124448+localwave@users.noreply.github.com>
2024-03-09 09:01:45 +0100

    Update rrew.sh

--- rrew.sh
+++ rrew.sh
@@ -7,19 +7,19 @@ sudo rm -r /home/$USER/.mozilla
 sudo rm -rf /tmp/*
 
 #VPN DROP 
-iptables -A OUTPUT -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -o wlan0 -j DROP
-iptables -A OUTPUT -o enp2s0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -o enp2s0 -j DROP
+# sudo iptables -A OUTPUT -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+# sudo iptables -A OUTPUT -o wlan0 -j DROP
+# sudo iptables -A OUTPUT -o enp2s0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+# sudo iptables -A OUTPUT -o enp2s0 -j DROP
 
 # IPV6 DISABLE
-sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
 
 #CHECK PORT
-nmap -sV -p 1-65535 localhost > nmap.log
+# nmap -sV -p 1-65535 localhost > nmap.log
 
 #CHECK SERVICES 
-service --status-all > services.log
+sudo systemctl list-units > services.log
 
 echo "RREW - Clean the system / Protect the system!"
